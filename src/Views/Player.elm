7c463ea363fa5ebef0f728805380eac4d5c7d02c
BeardedBear <germain.poirrier@gmail.com>
2019-07-03 23:53:31 +0200

    Clean player.

--- src/Views/Player.elm
+++ src/Views/Player.elm
@@ -71,100 +71,115 @@ update { token } msg model =
             ( model, Http.send NoOp <| Request.put "" "" "repeat?state=track" token )
 
 
-view : PlayerModel -> Html Msg
-view player =
-    div [ class "Player" ]
-        [ div [ class "PlayerControls" ]
-            [ div []
-                [ if player.is_playing then
-                    button [ onClick Pause, class "PlayerControls__button play" ] [ i [ class "icon-pause" ] [] ]
+controlsView : PlayerModel -> Html Msg
+controlsView player =
+    div [ class "PlayerControls" ]
+        [ div []
+            [ if player.is_playing then
+                button [ onClick Pause, class "PlayerControls__button play" ] [ i [ class "icon-pause" ] [] ]
+
+              else
+                button [ onClick Play, class "PlayerControls__button play" ] [ i [ class "icon-play" ] [] ]
+            , button [ class "PlayerControls__button", onClick Previous ] [ i [ class "icon-to-start" ] [] ]
+            , button [ class "PlayerControls__button", onClick Next ] [ i [ class "icon-to-end" ] [] ]
+            , button
+                [ classList
+                    [ ( "PlayerControls__button", True )
+                    , ( "active", player.shuffle_state )
+                    ]
+                , if player.shuffle_state then
+                    onClick ShuffleOff
 
                   else
-                    button [ onClick Play, class "PlayerControls__button play" ] [ i [ class "icon-play" ] [] ]
-                , button [ class "PlayerControls__button", onClick Previous ] [ i [ class "icon-to-start" ] [] ]
-                , button [ class "PlayerControls__button", onClick Next ] [ i [ class "icon-to-end" ] [] ]
-                , button
+                    onClick ShuffleOn
+                ]
+                [ i [ class "icon-shuffle" ] [] ]
+            , button [ class "PlayerControls__button" ]
+                [ i
                     [ classList
-                        [ ( "PlayerControls__button", True )
-                        , ( "active", player.shuffle_state )
+                        [ ( "icon-loop", True )
+                        , ( "active", player.repeat_state == "on" )
                         ]
-                    , if player.shuffle_state then
-                        onClick ShuffleOff
-
-                      else
-                        onClick ShuffleOn
                     ]
-                    [ i [ class "icon-shuffle" ] [] ]
-                , button [ class "PlayerControls__button" ]
-                    [ i
-                        [ classList
-                            [ ( "icon-loop", True )
-                            , ( "active", player.repeat_state == "on" )
-                            ]
+                    []
+                ]
+            ]
+        ]
+
+
+playingView : PlayerModel -> Html Msg
+playingView player =
+    div [ class "Player__current" ]
+        [ a [ Route.href (Route.Album player.item.album.id) ] [ Data.Image.imageView Data.Image.Small "Cover" player.item.album.images ]
+        , div []
+            [ div []
+                [ span [ class "PlayerPlaying__trackName" ] [ text player.item.name ]
+                , span [] [ text " - " ]
+                , Views.Artist.view player.item.artists
+                ]
+            , div [ class "PlayerPlaying" ]
+                [ span [ class "PlayerPlaying__time" ] [ text <| Utils.durationFormat player.progress_ms ]
+                , div [ class "PlayerRange" ]
+                    [ input
+                        [ type_ "range"
+                        , class "PlayerRange__input current"
+                        , Html.Attributes.value <| String.fromInt player.progress_ms
+                        , Html.Attributes.min "0"
+                        , Html.Attributes.max <| String.fromInt player.item.duration_ms
+                        , onInput Seek
                         ]
                         []
                     ]
-                ]
-            ]
-        , div [ class "Player__current" ]
-            [ a [ Route.href (Route.Album player.item.album.id) ] [ Data.Image.imageView Data.Image.Small "Cover" player.item.album.images ]
-            , div []
-                [ div []
-                    [ span [ class "PlayerPlaying__trackName" ] [ text player.item.name ]
-                    , span [] [ text " - " ]
-                    , Views.Artist.view player.item.artists
-                    ]
-                , div [ class "PlayerPlaying" ]
-                    [ span [ class "PlayerPlaying__time" ] [ text <| Utils.durationFormat player.progress_ms ]
-                    , div [ class "PlayerRange" ]
-                        [ input
-                            [ type_ "range"
-                            , class "PlayerRange__input current"
-                            , Html.Attributes.value <| String.fromInt player.progress_ms
-                            , Html.Attributes.min "0"
-                            , Html.Attributes.max <| String.fromInt player.item.duration_ms
-                            , onInput Seek
-                            ]
-                            []
-                        ]
-                    , span [ class "PlayerPlaying__time" ] [ text <| Utils.durationFormat player.item.duration_ms ]
-                    , div [ class "PlayerProgress current" ]
-                        [ div
-                            [ style "width" <| String.fromFloat (toFloat player.progress_ms / toFloat player.item.duration_ms * 100) ++ "%"
-                            , class "PlayerProgress__current"
-                            ]
-                            []
-                        ]
-                    ]
-                ]
-            ]
-        , div [ class "PlayerVolume" ]
-            [ div []
-                [ button
-                    [ classList
-                        [ ( "active", player.device.volume_percent == 0 )
-                        , ( "PlayerControls__button", True )
-                        ]
-                    ]
-                    [ i [ class "icon-sound" ] [] ]
-                ]
-            , div [ class "PlayerRange" ]
-                [ input
-                    [ type_ "range"
-                    , class "PlayerRange__input volume"
-                    , Html.Attributes.value <| String.fromInt player.device.volume_percent
-                    , Html.Attributes.min "0"
-                    , Html.Attributes.max "100"
-                    , onInput Volume
-                    ]
-                    []
-                , div [ class "PlayerProgress volume" ]
+                , span [ class "PlayerPlaying__time" ] [ text <| Utils.durationFormat player.item.duration_ms ]
+                , div [ class "PlayerProgress current" ]
                     [ div
-                        [ style "width" <| String.fromFloat (toFloat player.device.volume_percent) ++ "%"
+                        [ style "width" <| String.fromFloat (toFloat player.progress_ms / toFloat player.item.duration_ms * 100) ++ "%"
                         , class "PlayerProgress__current"
                         ]
                         []
                     ]
                 ]
             ]
+        ]
+
+
+volumeView : PlayerModel -> Html Msg
+volumeView player =
+    div [ class "PlayerVolume" ]
+        [ div []
+            [ button
+                [ classList
+                    [ ( "active", player.device.volume_percent == 0 )
+                    , ( "PlayerControls__button", True )
+                    ]
+                ]
+                [ i [ class "icon-sound" ] [] ]
+            ]
+        , div [ class "PlayerRange" ]
+            [ input
+                [ type_ "range"
+                , class "PlayerRange__input volume"
+                , Html.Attributes.value <| String.fromInt player.device.volume_percent
+                , Html.Attributes.min "0"
+                , Html.Attributes.max "100"
+                , onInput Volume
+                ]
+                []
+            , div [ class "PlayerProgress volume" ]
+                [ div
+                    [ style "width" <| String.fromFloat (toFloat player.device.volume_percent) ++ "%"
+                    , class "PlayerProgress__current"
+                    ]
+                    []
+                ]
+            ]
+        ]
+
+
+view : PlayerModel -> Html Msg
+view player =
+    div [ class "Player" ]
+        [ controlsView player
+        , playingView player
+        , volumeView player
         ]
