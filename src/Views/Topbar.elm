1da1172d9fc8a7e838456b341e8e5be1a68c2dfb
BeardedBear <germain.poirrier@gmail.com>
2019-11-08 01:09:54 +0100

    Init player design.

--- src/Views/Topbar.elm
+++ src/Views/Topbar.elm
@@ -8,7 +8,10 @@ import Views.Search as Search exposing (..)
 view : Html msg
 view =
     div [ class "Topbar" ]
-        [ div [ class "Sidebar__logo" ] [ text "LOGO" ]
+        [ div [ class "TopbarLogo" ]
+            [ img [ class "TopbarLogo__img", src "./img/logo.png" ] []
+            , span [ class "TopbarLogo__name" ] [ text "Beardify" ]
+            ]
         , Search.view
         , div [ class "User" ] [ text "Qui que je suis ?" ]
         ]
