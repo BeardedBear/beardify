8f2a91fb7b9925e43d0cdd8d91226738223cb060
Fabrice Bentz <fabrice@weeple.fr>
2019-11-08 17:53:49 +0100

    Remove bottom bar component.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -3,7 +3,8 @@ module Page.Home exposing (Model, Msg(..), init, update, view)
 import Data.Session exposing (Session)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Views.BottomBar as BottomBar
+import Views.Device as Device
+import Views.Player as Player
 import Views.Sidebar as Sidebar
 import Views.Topbar as Topbar
 
@@ -45,7 +46,10 @@ view _ _ =
                     [ h1 [] [ text "Ceci est une page d'accueil du plus bel effet." ]
                     , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget convallis justo, eget sodales eros. Curabitur massa lacus, convallis non metus pretium, iaculis venenatis tellus. Morbi faucibus scelerisque ex, non ullamcorper lorem semper non. Nunc sapien turpis, finibus ac egestas eget, aliquam at magna. Cras bibendum sit amet elit sed ornare. Maecenas et neque scelerisque, interdum dui eu, tincidunt augue. Pellentesque vitae tincidunt nisl, id maximus massa. Cras tempor, orci at gravida aliquam, nunc elit porta augue, a varius arcu odio vitae risus. Curabitur eget leo tristique, laoreet libero et, posuere justo. Phasellus non consectetur dolor. Suspendisse ac auctor nisi. Donec rhoncus neque at tortor euismod, at semper nunc bibendum. Nulla in efficitur lacus, sit amet interdum felis. Donec id est vehicula odio feugiat suscipit quis congue quam. Donec eu purus tristique, tincidunt quam vitae, lobortis lacus." ]
                     ]
-                , BottomBar.view
+                , div [ class "BottomBar" ]
+                    [ Player.view
+                    , Device.view
+                    ]
                 ]
             ]
       ]
