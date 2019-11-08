79fec7ff243e2851a98ebf88bb299c564b73a2b6
BeardedBear <germain.poirrier@gmail.com>
2019-11-08 23:00:56 +0100

    Add beautiful player.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -46,7 +46,7 @@ view _ _ =
                     [ h1 [] [ text "Ceci est une page d'accueil du plus bel effet." ]
                     , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget convallis justo, eget sodales eros. Curabitur massa lacus, convallis non metus pretium, iaculis venenatis tellus. Morbi faucibus scelerisque ex, non ullamcorper lorem semper non. Nunc sapien turpis, finibus ac egestas eget, aliquam at magna. Cras bibendum sit amet elit sed ornare. Maecenas et neque scelerisque, interdum dui eu, tincidunt augue. Pellentesque vitae tincidunt nisl, id maximus massa. Cras tempor, orci at gravida aliquam, nunc elit porta augue, a varius arcu odio vitae risus. Curabitur eget leo tristique, laoreet libero et, posuere justo. Phasellus non consectetur dolor. Suspendisse ac auctor nisi. Donec rhoncus neque at tortor euismod, at semper nunc bibendum. Nulla in efficitur lacus, sit amet interdum felis. Donec id est vehicula odio feugiat suscipit quis congue quam. Donec eu purus tristique, tincidunt quam vitae, lobortis lacus." ]
                     ]
-                , div [ class "BottomBar" ]
+                , div [ class "Content__bottom" ]
                     [ Player.view
                     , Device.view
                     ]
