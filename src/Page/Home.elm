afe635a590ea56410dac2494c9b99a4d5c9d8895
BeardedBear <germain.poirrier@gmail.com>
2019-11-08 10:18:03 +0100

    Add player elements.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -3,7 +3,7 @@ module Page.Home exposing (Model, Msg(..), init, update, view)
 import Data.Session exposing (Session)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Views.Player as Player
+import Views.BottomBar as BottomBar
 import Views.Sidebar as Sidebar
 import Views.Topbar as Topbar
 
@@ -45,7 +45,7 @@ view _ _ =
                     [ h1 [] [ text "Ceci est une page d'accueil du plus bel effet." ]
                     , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget convallis justo, eget sodales eros. Curabitur massa lacus, convallis non metus pretium, iaculis venenatis tellus. Morbi faucibus scelerisque ex, non ullamcorper lorem semper non. Nunc sapien turpis, finibus ac egestas eget, aliquam at magna. Cras bibendum sit amet elit sed ornare. Maecenas et neque scelerisque, interdum dui eu, tincidunt augue. Pellentesque vitae tincidunt nisl, id maximus massa. Cras tempor, orci at gravida aliquam, nunc elit porta augue, a varius arcu odio vitae risus. Curabitur eget leo tristique, laoreet libero et, posuere justo. Phasellus non consectetur dolor. Suspendisse ac auctor nisi. Donec rhoncus neque at tortor euismod, at semper nunc bibendum. Nulla in efficitur lacus, sit amet interdum felis. Donec id est vehicula odio feugiat suscipit quis congue quam. Donec eu purus tristique, tincidunt quam vitae, lobortis lacus." ]
                     ]
-                , Player.view
+                , BottomBar.view
                 ]
             ]
       ]
