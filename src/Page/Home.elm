750f45430d9874ac9a503b45e2940c02c33a24e6
BeardedBear <germain.poirrier@gmail.com>
2019-11-10 15:18:42 +0100

    Beautiful sidebar.

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -36,15 +36,17 @@ update session msg model =
 
 
 view : Session -> Model -> ( String, List (Html Msg) )
-view _ _ =
+view session _ =
     ( "Home"
-    , [ Topbar.view
+    , [ Topbar.view session
       , div [ class "App__body" ]
             [ Sidebar.view
             , div [ class "Content" ]
-                [ div [ class "Page" ]
-                    [ h1 [] [ text "Ceci est une page d'accueil du plus bel effet." ]
-                    , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget convallis justo, eget sodales eros. Curabitur massa lacus, convallis non metus pretium, iaculis venenatis tellus. Morbi faucibus scelerisque ex, non ullamcorper lorem semper non. Nunc sapien turpis, finibus ac egestas eget, aliquam at magna. Cras bibendum sit amet elit sed ornare. Maecenas et neque scelerisque, interdum dui eu, tincidunt augue. Pellentesque vitae tincidunt nisl, id maximus massa. Cras tempor, orci at gravida aliquam, nunc elit porta augue, a varius arcu odio vitae risus. Curabitur eget leo tristique, laoreet libero et, posuere justo. Phasellus non consectetur dolor. Suspendisse ac auctor nisi. Donec rhoncus neque at tortor euismod, at semper nunc bibendum. Nulla in efficitur lacus, sit amet interdum felis. Donec id est vehicula odio feugiat suscipit quis congue quam. Donec eu purus tristique, tincidunt quam vitae, lobortis lacus." ]
+                [ div [ class "Page HelperScrollArea" ]
+                    [ div [ class "Page__body HelperScrollArea__target" ]
+                        [ h1 [ class "Heading first" ] [ text "Ceci est une page d'accueil du plus bel effet." ]
+                        , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget convallis justo, eget sodales eros. Curabitur massa lacus, convallis non metus pretium, iaculis venenatis tellus. Morbi faucibus scelerisque ex, non ullamcorper lorem semper non. Nunc sapien turpis, finibus ac egestas eget, aliquam at magna. Cras bibendum sit amet elit sed ornare. Maecenas et neque scelerisque, interdum dui eu, tincidunt augue. Pellentesque vitae tincidunt nisl, id maximus massa. Cras tempor, orci at gravida aliquam, nunc elit porta augue, a varius arcu odio vitae risus. Curabitur eget leo tristique, laoreet libero et, posuere justo. Phasellus non consectetur dolor. Suspendisse ac auctor nisi. Donec rhoncus neque at tortor euismod, at semper nunc bibendum. Nulla in efficitur lacus, sit amet interdum felis. Donec id est vehicula odio feugiat suscipit quis congue quam. Donec eu purus tristique, tincidunt quam vitae, lobortis lacus." ]
+                        ]
                     ]
                 , div [ class "Content__bottom" ]
                     [ Player.view
