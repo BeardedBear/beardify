057464f525dc0c55feb98a202d615e482ff57910
BeardedBear <germain.poirrier@gmail.com>
2019-11-17 00:07:12 +0100

    Add modal.

--- src/Views/Page.elm
+++ src/Views/Page.elm
@@ -21,6 +21,21 @@ frame { session, clearNotification } ( title, content ) =
             { clear = clearNotification
             , notifs = session.notifications
             }
+        , div [ class "Modal" ]
+            [ div [ class "Modal__form" ]
+                [ div [ class "Modal__head" ]
+                    [ h2 [ class "Heading second" ] [ text "Faire des machins" ]
+                    , button [ class "Button nude" ] [ i [ class "icon-close" ] [] ]
+                    ]
+                , div [ class "Modal__body " ]
+                    [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, vulputate rhoncus quam sit amet, cursus accumsan ex. Suspendisse aliquam sapien sapien, quis dapibus arcu cursus dictum. Donec faucibus sit amet diam vel aliquam. Praesent purus dolor, malesuada quis ex vel, pulvinar blandit dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, vulputate rhoncus quam sit amet, cursus accumsan ex. Suspendisse aliquam sapien sapien, quis dapibus arcu cursus dictum. Donec faucibus sit amet diam vel aliquam. Praesent purus dolor, malesuada quis ex vel, pulvinar blandit dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, vulputate rhoncus quam sit amet, cursus accumsan ex. Suspendisse aliquam sapien sapien, quis dapibus arcu cursus dictum. Donec faucibus sit amet diam vel aliquam. Praesent purus dolor, malesuada quis ex vel, pulvinar blandit dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, vulputate rhoncus quam sit amet, cursus accumsan ex. Suspendisse aliquam sapien sapien, quis dapibus arcu cursus dictum. Donec faucibus sit amet diam vel aliquam. Praesent purus dolor, malesuada quis ex vel, pulvinar blandit dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, vulputate rhoncus quam sit amet, cursus accumsan ex. Suspendisse aliquam sapien sapien, quis dapibus arcu cursus dictum. Donec faucibus sit amet diam vel aliquam. Praesent purus dolor, malesuada quis ex vel, pulvinar blandit dui.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris neque tortor, vulputate rhoncus quam sit amet, cursus accumsan ex. Suspendisse aliquam sapien sapien, quis dapibus arcu cursus dictum. Donec faucibus sit amet diam vel aliquam. Praesent purus dolor, malesuada quis ex vel, pulvinar blandit dui."
+                    ]
+                , div [ class "Modal__foot" ]
+                    [ button [ class "Button" ] [ text "Cancel" ]
+                    , button [ class "Button primary" ] [ text "Accept" ]
+                    ]
+                ]
+            ]
         , main_ [ class "App" ] content
         ]
     }
