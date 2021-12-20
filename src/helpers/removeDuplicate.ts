e2191ff372242242c04b4f5e832319e751f2f557
BeardedBear <germain.poirrier@gmail.com>
2021-12-20 20:12:00 +0100

    Simplify removeduplicate fn

--- src/helpers/removeDuplicate.ts
+++ src/helpers/removeDuplicate.ts
@@ -2,18 +2,7 @@ import { AlbumSimplified } from "../@types/Album";
 
 export function removeDuplicatesAlbums(array: AlbumSimplified[]): AlbumSimplified[] {
   return array.reduce((acc: AlbumSimplified[], value) => {
-    return acc.some(
-      (album) =>
-        album.name.toLowerCase() === value.name.toLowerCase() ||
-        album.name
-          .replaceAll(/[^a-z0-9]+/gi, " ")
-          .toLowerCase()
-          .replaceAll(" ", "") ===
-          value.name
-            .replaceAll(/[^a-z0-9]+/gi, " ")
-            .toLowerCase()
-            .replaceAll(" ", ""),
-    )
+    return acc.some((album) => album.name.trim().toLowerCase() === value.name.trim().toLowerCase())
       ? acc
       : acc.concat(value);
   }, []);
