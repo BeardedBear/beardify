828acdb810a443ff6c89f7e7d1ac0d3ce9272b5d
BeardedBear <germain.poirrier@gmail.com>
2022-03-26 19:11:34 +0100

    Change directus source

--- src/views/releases/ReleasesStore.ts
+++ src/views/releases/ReleasesStore.ts
@@ -29,12 +29,12 @@ export const useReleases = defineStore("releases", {
       const authStore = useAuth();
       const userId = authStore.me?.id;
       const { data } = await axios.get<{ data: ReleasesCheck[] }>(
-        `https://beardictus.com/items/ReleasesCheck?filter[user][_eq]=${userId}`,
+        `https://2fpx4328.directus.app/items/releases_check?filter[user][_eq]=${userId}`,
       );
 
       if (!data.data.length) {
         axios
-          .post("https://beardictus.com/items/ReleasesCheck", { user: useAuth().me?.id, checks: [] })
+          .post("https://2fpx4328.directus.app/items/releases_check", { user: useAuth().me?.id, checks: [] })
           .then(() => (this.checks = []));
       } else {
         this.checks = data.data[0].checks;
@@ -48,39 +48,41 @@ export const useReleases = defineStore("releases", {
       const allreadyExist = this.checks?.some((r) => r.id === releaseId);
       const checks = !allreadyExist ? addChecks : delChecks;
 
-      axios.patch(`https://beardictus.com/items/ReleasesCheck/${this.uid}`, { checks });
+      axios.patch(`https://2fpx4328.directus.app/items/releases_check/${this.uid}`, { checks });
       this.checks = checks;
     },
 
     async getReleases() {
-      axios.get<{ data: { releases: Release[] } }>("https://beardictus.com/items/releases").then(({ data }) => {
-        const categories = Array.from(new Set(data.data.releases.map((release) => release.category)));
-        function getSlugsByCategory(category: string): string[] {
-          const array: string[] = [];
-          const mergedSlugs = data.data.releases
-            .filter((release) => release.category === category)
-            .map((e) => array.concat(e.slug))
-            .flat();
-          return Array.from(new Set(mergedSlugs));
-        }
+      axios
+        .get<Release[]>("https://2fpx4328.directus.app/assets/6d6a9ed6-64ba-4724-a583-fc77b0ca039a")
+        .then(({ data }) => {
+          const categories = Array.from(new Set(data.map((release) => release.category)));
+          function getSlugsByCategory(category: string): string[] {
+            const array: string[] = [];
+            const mergedSlugs = data
+              .filter((release) => release.category === category)
+              .map((e) => array.concat(e.slug))
+              .flat();
+            return Array.from(new Set(mergedSlugs));
+          }
 
-        categories.forEach((category): void => {
-          const teee: MenuItem = {
-            name: category,
-            slugs: getSlugsByCategory(category),
-          };
-          if (!this.menu.find((e) => e.name == teee.name)) this.menu.push(teee);
+          categories.forEach((category): void => {
+            const teee: MenuItem = {
+              name: category,
+              slugs: getSlugsByCategory(category),
+            };
+            if (!this.menu.find((e) => e.name == teee.name)) this.menu.push(teee);
+          });
+
+          const dataWithMergedSlugs = useMergeReleaseSlugs(data);
+          const removedLives = dataWithMergedSlugs.filter((release) => !useCheckLiveAlbum(release.album));
+          const removedReissues = removedLives.filter((release) => !useCheckReissueAlbum(release.album));
+
+          this.monthList = Array.from(new Set(dataWithMergedSlugs.map((release) => release.releaseDate))).sort(
+            (a, b) => Number(b.split(" ").pop()) - Number(a.split(" ").pop()),
+          );
+          this.releases = removedReissues.sort((a, b) => b.releaseDateRaw - a.releaseDateRaw);
         });
-
-        const dataWithMergedSlugs = useMergeReleaseSlugs(data.data.releases);
-        const removedLives = dataWithMergedSlugs.filter((release) => !useCheckLiveAlbum(release.album));
-        const removedReissues = removedLives.filter((release) => !useCheckReissueAlbum(release.album));
-
-        this.monthList = Array.from(new Set(dataWithMergedSlugs.map((release) => release.releaseDate))).sort(
-          (a, b) => Number(b.split(" ").pop()) - Number(a.split(" ").pop()),
-        );
-        this.releases = removedReissues.sort((a, b) => b.releaseDateRaw - a.releaseDateRaw);
-      });
     },
   },
 });
