54c2ca50204804e2d8c43282c0817b81b1612434
BeardedBear <germain.poirrier@gmail.com>
2026-05-14 18:59:52 +0200

    Init update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,6 +33,7 @@ fn open_spotify_auth(_app: AppHandle, url: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![set_play_state, open_spotify_auth])
